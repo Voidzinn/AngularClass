ce3f5711df1fdcac4ae924ae368a98331b8900f1
Voidzinn <voidboyyy@gmail.com>
2025-09-01 20:32:09 -0300

    Adicionando rotas e conteúdos dentro do home.html

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Calculadora } from './calculadora/calculadora';
+import { ListarCliente } from './listar-cliente/listar-cliente';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: Home},
+  {path: 'calculadora', component: Calculadora},
+  {path: 'listar-cliente', component: ListarCliente}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
