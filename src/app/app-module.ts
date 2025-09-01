b67aa13b81ba8afb998320fd322b65a9061ea374
Voidzinn <voidboyyy@gmail.com>
2025-09-01 20:13:26 -0300

    Initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,35 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { MenuSuperior } from './menu-superior/menu-superior';
+import { Noticia } from './noticia/noticia';
+import { Destaque } from './destaque/destaque';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListarCliente } from './listar-cliente/listar-cliente';
+import { Home } from './home/home';
+import { Calculadora } from './calculadora/calculadora';
+
+@NgModule({
+  declarations: [
+    App,
+    MenuSuperior,
+    Noticia,
+    Destaque,
+    ListarCliente,
+    Home,
+    Calculadora
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    NgbCarouselModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
